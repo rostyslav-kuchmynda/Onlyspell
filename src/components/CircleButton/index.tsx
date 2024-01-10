12b2d7388235ddc7131042d6c7d798f65ac9cd7f
Rostyslav Kuchmynda <rostyk2994@gmail.com>
2024-01-10 21:42:47 +0000

    Circle btn fix

--- src/components/CircleButton/index.tsx
+++ src/components/CircleButton/index.tsx
@@ -1,11 +1,14 @@
 import classes from './styles.module.scss';
 
+import ArrowRightIcon from '../../assets/DropDownButton/arrow_right.svg';
+
 type CircleButtonType = {
   text: string;
 };
 
 export const CircleButton: React.FC<CircleButtonType> = ({ text }) => (
-  <button className={classes.dropDownButtonContainer}>
+  <button className={classes.circleButtonContainer}>
     <span>{text}</span>
+    <img className={classes.rightArrowIcon} src={ArrowRightIcon} />
   </button>
 );
